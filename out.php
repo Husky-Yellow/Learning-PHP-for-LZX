c204ded08275c4da4f3d375dc38d0908fa076752
哈士奇-黄 <50540342+Husky-Yellow@users.noreply.github.com>
2020-02-24 17:58:45 +0800

    补上
    补上

---
+++ out.php
@@ -0,0 +1,18 @@
+<?php
+echo "<h2>PHP是世界上最好的语言</h2>";
+echo "Hello World!<br>";
+echo "我要学PHP<br>";
+echo "这是一个", "字符串<br>";
+
+$txt1 = "学习PHP";
+$txt2 = "RUNOOB.COM";
+$cars = array("Volvo", "BMW", "Toyota");
+
+echo $txt1;
+echo "<br>";
+echo "在 $txt2 学习PHP";
+echo "<br>";
+echo "我的汽车品牌是 {$cars[0]}";
+
+print "<h1>PHP很有趣！</h1>";
+print $txt1;
